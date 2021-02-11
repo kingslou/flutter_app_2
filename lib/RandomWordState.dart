23e71e8703bf8621abf65728fc45d88665a116ba
geen <loujin_007@126.com>
2021-02-11 14:27:51 +0800

    First 列表搜藏

--- lib/RandomWordState.dart
+++ lib/RandomWordState.dart
@@ -6,39 +6,55 @@ import 'package:flutter_app_2/main.dart';
 class RandomWordState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    final _suggestions = <WordPair>[];
-    final _bigFont = const TextStyle(fontSize: 18.0);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('xxxx'),
+      ),
+      body: _buildList(),
+    );
+  }
 
-    // 对于Flutter 全是widget
-    Widget _buildRow(WordPair wordPair){
-      return new ListTile(
-          title: new Text(
-              wordPair.asPascalCase,
-              style: _bigFont,
-          ),
-      );
-    }
+  final _suggestions = <WordPair>[];
+  final _bigFont = const TextStyle(fontSize: 18.0);
+  //收藏列表
+  final _saved = new Set<WordPair>();
+  // 对于Flutter 全是widget
+  Widget _buildRow(WordPair wordPair) {
+    final _alreadySaved = _saved.contains(wordPair);
+    return new ListTile(
+      title: new Text(
+        wordPair.asPascalCase,
+        style: _bigFont,
+      ),
+      trailing: new Icon(
+        _alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: _alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) {
+            _saved.remove(wordPair);
+          } else {
+            _saved.add(wordPair);
+          }
+        });
+      },
+    );
+  }
 
-    Widget _buildList(){
-      return new ListView.builder(
+  Widget _buildList() {
+    return new ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context,i){
-          if(i.isOdd){return new Divider();}
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return new Divider();
+          }
           // ~/2 向下取整
           final index = i ~/ 2;
-          if(index>=_suggestions.length){
+          if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
-        }
-      );
-    }
-
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('xxxx'),),
-      body:_buildList() ,
-    );
+        });
   }
 }
